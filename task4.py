71bfcb6e1e1e56aaf21aed7dbcd2279b8619171e
sambernn <55156541+sambernn@users.noreply.github.com>
2020-09-28 12:13:48 -0700

    Update task4.py

--- task4.py
+++ task4.py
@@ -9,3 +9,12 @@
 # Outputs:
 # "the sentence contains password"
 # "the sentence does not contain password"
+
+print("a sentence")
+x = input()
+x = str(x)
+
+if "password" in x: 
+    print("the sentence contains password")
+else:
+    print("the sentence does not contain password")
