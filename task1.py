9ad85bb13fffeac6417637b8b1c1077c69ebcebe
sambernn <55156541+sambernn@users.noreply.github.com>
2020-09-28 11:34:08 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,13 +1,11 @@
-#! python3
-
-# Have the user input a number. 
-# Determine if the number is larger than 100 
-# If it is, the output should read "The number is larger than 100" 
-# (2 points)
-# Inputs:
-# number
-
-# Outputs:
-# "The number is larger than 100"
-# "The number is smaller than 100"
-# "The number is 100"
+print("Input a number")
+x = input()
+x = int(x)
+y = 100
+y = int(y)
+if x > y:
+    print("the number is larger than 100")
+if x < y:
+    print("the number is smaller than 100")
+if x == y:
+    print("the number is 100")
