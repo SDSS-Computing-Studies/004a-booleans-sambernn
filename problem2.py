b9e9fc9ebbbe70b3cc022570f2a433e16c953ff3
sambernn <55156541+sambernn@users.noreply.github.com>
2020-09-28 12:54:14 -0700

    y

--- problem2.py
+++ problem2.py
@@ -10,3 +10,13 @@
 # Outputs:
 # "the number is an integer"
 # "the number is not an integer"
+
+import math
+print("enter a number")
+x = input()
+x = float(x)
+x2 = math.floor(x)
+if x == x2:
+    print('the number is an integer')
+else:
+    print('the number is not an integer')
