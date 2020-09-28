e423f24e232dac01dd19b8c3a8947974b4348b32
sambernn <55156541+sambernn@users.noreply.github.com>
2020-09-28 12:08:14 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,19 +1,12 @@
-#! python3 
+print("Input a number")
+x = input()
+x = float(x)
+if x >= 1000:
+    print("The number is equal to 1000 or is larger than 1000")
+elif x >= 100 < 1000:
+    print("The number is 100 or a number up to 1000")
+elif x >= 10 < 100:
+    print("The number is 10 or a number up to 100")
+elif x >= 0 < 10:
+    print("The number is 0 or a number up to 10")
 
-# Have the user enter a number. 
-# Use an if...elif statement to determine if the number is 
-# a) larger than 1000 
-# b) larger than 100 
-# c) larger than 10 
-# d) larger than 0 
-# Output must match one of the valid output statements listed
-# (2 points)
-
-# Inputs:
-# a number
-
-# Output is a single number that represents a range of numbers:
-# "3" : The number is equal to 1000 or is larger than 1000
-# "2" : The number is 100 or a number up to 1000 
-# "1" : The number is 10 or a number up to 100 
-# "0" : The number is 0 or a number up to 100 
